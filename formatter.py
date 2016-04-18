4cd57291311f48f6d523543a704deca6a7758037
bpmagallon <bpmagallon@gmail.com>
2016-04-18 23:17:39 +0900

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -7,22 +7,40 @@ def group(arr):
     tle2 = arr[2]
     tle1 = re.sub(r"\s{1,}", ",", tle1)
     tle1_split = tle1.split(",")
-    bstar = tle1_split[6]
+
+    #epoch
+    epoch = tle1_split[3]
+    
+    #drag value
+    bstar = tle1_split[6] 
 
     if bstar[0]=='+':
         bstar = float("0."+bstar[1:6])/(10**float(bstar[7]))
-    
+    else:
+        bstar = float("0."+bstar[1:6])/(10**float(bstar[7]))*-1
+        
     tle2 = re.sub(r"\s{1,}", ",", tle2)
     tle2_split = tle2.split(",")
-    n = tle2_split[7]
+
+    #mean motion
+    n = tle2_split[7] 
 
     n = float(n[:11])
     n = n*((2*pi)/(1440))
+
+    #inclination
+    i = radians(float(tle2_split[2]))
     
-    i = float(tle2_split[2])
+    #raan
+    node = float(tle2_split[3])
+    
+    #eccentricity
     e = float("0."+tle2_split[4])
 
-    i = radians(i)
+    #argument of perigee
     omega = radians(float(tle2_split[5]))
+
+    #mean anomaly
+    m = radians(float(tle2_split[6]))
     
-    return n,i,e,bstar,omega
+    return epoch, n, i, node, e, omega, m, bstar
